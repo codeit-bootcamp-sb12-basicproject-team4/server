199706c14d5a5942385214b103666825557670a9
seongjuun <beajon0301@naver.com>
2026-05-15 14:30:30 +0900

    init: git init

---
+++ src/main/java/com/codeit/findex/FindexApplication.java
@@ -0,0 +1,15 @@
+package com.codeit.findex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class FindexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindexApplication.class, args);
+	}
+
+}
